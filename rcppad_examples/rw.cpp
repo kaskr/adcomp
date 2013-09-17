989e89a365c315a52d8bc2a3f743ecec43753f60
casperwberg <casperwberg@gmail.com>
2013-09-17 10:04:02 +0200

    update slideshow

--- rcppad_examples/rw.cpp
+++ rcppad_examples/rw.cpp
@@ -18,17 +18,19 @@ Type objective_function<Type>::operator() ()
   Type rho=f(transf_rho);
   vector<Type> sds=exp(logsds);
   vector<Type> sdObs=exp(logsdObs);
+  // Setup object for evaluating multivariate normal likelihood
   matrix<Type> cov(stateDim,stateDim);
   for(int i=0;i<stateDim;i++)
     for(int j=0;j<stateDim;j++)
       cov(i,j)=pow(rho,Type(abs(i-j)))*sds[i]*sds[j];
   using namespace density;
   MVNORM_t<Type> neg_log_density(cov);
+  /* Define likelihood */
   Type ans=0;
   ans-=dnorm(vector<Type>(u(0)),Type(0),Type(1),1).sum();
+  for(int i=1;i<timeSteps;i++)    
+    ans+=neg_log_density(u(i)-u(i-1)); // Process likelihood 
   for(int i=1;i<timeSteps;i++)
-    ans+=neg_log_density(u(i)-u(i-1));
-  for(int i=1;i<timeSteps;i++)
-    ans-=dnorm(vector<Type>(obs(i)),vector<Type>(u(i)),sdObs,1).sum();
+    ans-=dnorm(vector<Type>(obs(i)),vector<Type>(u(i)),sdObs,1).sum(); // Data likelihood
   return ans;
 }
