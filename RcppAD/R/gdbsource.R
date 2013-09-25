4266a2037dcb1255d0ac575a10608991646a3453
Kasper Kristensen <kaskr@imm.dtu.dk>
2013-09-25 09:13:53 +0200

    gdbsource interactive

--- RcppAD/R/gdbsource.R
+++ RcppAD/R/gdbsource.R
@@ -4,18 +4,32 @@
 ##' If a script aborts e.g. due to an out-of-bound index operation
 ##' it should be fast to locate the line that caused the problem by
 ##' running \code{gdbsource(file)}.
+##' Alternatively, If more detailed debugging is required,  then
+##' \code{gdbsource(file,TRUE)} will provide the full backtrace followed
+##' by an interactive gdb session where the individual frames can be inspected.
 ##' @title Source R-script through gdb to get backtrace.
 ##' @param file Your R script
+##' @param interactive Run interactive gdb session?
 ##' @return Object of class \code{backtrace}
-gdbsource <- function(file){
+gdbsource <- function(file,interactive=FALSE){
   gdbscript <- tempfile()
-  cat("run\nbt\nquit\n",file=gdbscript)
-  cmd <- paste("R --vanilla < ",file," -d gdb --debugger-args=\"-x",
-               gdbscript,"\"")
-  txt <- system(cmd,intern=TRUE,ignore.stdout=FALSE,ignore.stderr=TRUE)
-  attr(txt,"file") <- file
-  class(txt) <- "backtrace"
-  txt
+  if(interactive){
+    gdbcmd <- c(paste("run --vanilla <",file),
+                "bt")
+    gdbcmd <- paste(gdbcmd,"\n",collapse="")
+    cat(gdbcmd,file=gdbscript)
+    cmd <- paste("R -d gdb --debugger-args=\"-x",gdbscript,"\"")
+    system(cmd,intern=FALSE,ignore.stdout=FALSE,ignore.stderr=TRUE)
+    return(NULL)
+  } else {
+    cat("run\nbt\nquit\n",file=gdbscript)
+    cmd <- paste("R --vanilla < ",file," -d gdb --debugger-args=\"-x",
+                 gdbscript,"\"")
+    txt <- system(cmd,intern=TRUE,ignore.stdout=FALSE,ignore.stderr=TRUE)
+    attr(txt,"file") <- file
+    class(txt) <- "backtrace"
+    return(txt)
+  }
 }
 print.backtrace <- function(x,...){
   ## Backtrace begins here
