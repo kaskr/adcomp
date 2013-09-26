d9142ab8f718c86a24557735f6e3c5373d7bad2d
hskaug <hskaug@linuxbox.(none)>
2013-09-26 12:18:16 +0200

    Scaled up Orange Tree example from ADMB example collection

---
+++ rcppad_examples/orange_big.R
@@ -0,0 +1,23 @@
+# Scaled up version of the Orange Tree example (50,000 latent random variables)
+
+require(RcppAD)
+
+# Read data
+source("data_orange.R")
+
+compile("orange_big.cpp")
+dyn.load("orange_big.so")
+Mmultiply = data_orange$M*data_orange$multiply
+
+obj <- MakeADFun(data=data_orange,
+                 parameters=list(
+		   beta=c(0,0,0),
+		   log_sigma=1,
+		   log_sigma_u=2,
+                   u = rep(0,Mmultiply)),	
+                 random=c("^u")
+		 )
+obj$control <- list(trace=1,parscale=c(1,1,1,1,1)*1e-2,REPORT=1,reltol=1e-12,maxit=100)
+obj$hessian <- F
+newtonOption(smartsearch=TRUE)
+tid = system.time(opt<-nlminb(obj$par,obj$fn,obj$gr,lower=c(-10.0,-10,-10,-5,-5),upper=c(10.0,10,10,5.0,5.0)))
---
+++ rcppad_examples/orange_big.cpp
@@ -0,0 +1,54 @@
+// Scaled up version of the Orange Tree example (50,000 latent random variables)
+#include <RcppAD.hpp>
+
+template<class Type>
+Type objective_function<Type>::operator() ()
+{
+  DATA_INTEGER(n);
+  DATA_VECTOR(y);
+  DATA_VECTOR(t);
+  DATA_INTEGER(M);
+  DATA_FACTOR(ngroup);
+  DATA_INTEGER(multiply);
+  PARAMETER_VECTOR(beta);
+  PARAMETER(log_sigma);
+  PARAMETER(log_sigma_u);
+  PARAMETER_VECTOR(u);
+
+  Type sigma=exp(log_sigma);
+  Type sigma_u=exp(log_sigma_u);
+
+  using namespace density;
+  int i,j,k,ii;
+
+  parallel_accumulator<Type> g(this);
+
+  for(k=0;k< multiply;k++)
+  {
+    ii = 0;
+    for(i=0;i< M;i++)
+    {
+      // Random effects contribution
+      Type u1 = u[i+k*M];
+      g -= -.5*u1*u1;
+
+      vector<Type> a(3); 
+      a[0] = 192.0 + beta[0] + sigma_u*u1;			
+      a[1] = 726.0 + beta[1];
+      a[2] = 356.0 + beta[2];
+
+      Type tmp;
+      Type f;
+
+      for(j=0;j<ngroup(i);j++)
+      {
+        f = a[0]/(1+exp(-(t[ii]-a[1])/a[2]));
+        tmp = (y[ii] - f)/sigma;
+        g -= -log_sigma - 0.5*tmp*tmp;
+        ii++;
+      }
+    }
+  }
+
+  return g;
+}
