b4f3684091318e20d2588c09b27e5ee3cb947dd4
Kasper Kristensen <kaskr@imm.dtu.dk>
2013-10-13 09:43:26 +0200

    new example

---
+++ rcppad_examples/rw_sparse.cpp
@@ -0,0 +1,32 @@
+// Identical with random walk example. Utilizing sparse block structure so efficient when the number of states is high.
+#include <RcppAD.hpp>
+
+/* Parameter transform */
+template <class Type>
+Type f(Type x){return Type(2)/(Type(1) + exp(-Type(2) * x)) - Type(1);}
+
+template<class Type>
+Type objective_function<Type>::operator() ()
+{
+  DATA_ARRAY(obs); /* timeSteps x stateDim */
+  PARAMETER_ARRAY(u); /* State */
+  PARAMETER(transf_rho);
+  PARAMETER_VECTOR(logsds);
+  PARAMETER_VECTOR(logsdObs);
+  int timeSteps=obs.dim[0];
+  int stateDim=obs.dim[1];
+  Type rho=f(transf_rho);
+  vector<Type> sds=exp(logsds);
+  vector<Type> sdObs=exp(logsdObs);
+  // Setup object for evaluating multivariate normal likelihood
+  using namespace density;
+  VECSCALE_t<AR1_t<N01<Type> > > neg_log_density=VECSCALE(AR1(rho),sds);
+  /* Define likelihood */
+  Type ans=0;
+  ans-=dnorm(vector<Type>(u(0)),Type(0),Type(1),1).sum();
+  for(int i=1;i<timeSteps;i++)    
+    ans+=neg_log_density(u(i)-u(i-1)); // Process likelihood 
+  for(int i=1;i<timeSteps;i++)
+    ans-=dnorm(vector<Type>(obs(i)),vector<Type>(u(i)),sdObs,1).sum(); // Data likelihood
+  return ans;
+}
