4e7c2adeab1d60c7113370060f562cadd970c065
Kasper Kristensen <kaskr@imm.dtu.dk>
2014-02-10 11:45:22 +0100

    Compact implementation of sdv_multi example

---
+++ tmb_examples/sdv_multi_compact.cpp
@@ -0,0 +1,33 @@
+// Compact version of sdv_multi
+#include <TMB.hpp>
+
+template<class Type>
+Type objective_function<Type>::operator() ()
+{
+  DATA_INTEGER(n);
+  DATA_INTEGER(p);
+  DATA_ARRAY(y);
+  PARAMETER_VECTOR(phi);
+  PARAMETER_VECTOR(log_sigma);
+  PARAMETER_VECTOR(mu_x);
+  PARAMETER_VECTOR(off_diag_x);
+  PARAMETER_ARRAY(h);
+
+  using namespace density;
+
+  Type g=0;
+  vector<Type> sigma=exp(log_sigma);
+  array<Type> ht=h.transpose(); // For row access
+  vector<Type> sigma_init=sigma/sqrt(Type(1.0)-phi*phi); // Initial sd of AR1
+  for(int j=0;j<p;j++)g += SCALE(AR1(phi(j)),sigma_init(j))(ht.col(j));
+
+  // Likelihood contribution: observations
+  UNSTRUCTURED_CORR_t<Type> neg_log_density(off_diag_x);
+  for(int i=0;i<n;i++)
+  {
+    vector<Type> sigma_y = exp(Type(0.5)*(mu_x + vector<Type>(h.col(i))));
+    g += VECSCALE(neg_log_density,sigma_y)(vector<Type>(y.col(i)));
+  }
+
+  return g;
+}
