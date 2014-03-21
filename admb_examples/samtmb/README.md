5f1a0eb41d6952ff011a8d2c31868ba48c6cb336
Anders Nielsen <andni@aqua-cb-andni11.(none)>
2014-03-21 21:08:26 +0100

    First version of SAM in TMB and SAM in ADMB to compare

---
+++ admb_examples/samtmb/README.md
@@ -0,0 +1,11 @@
+README
+======
+
+For this model to run you need the following command line options:
+
+./sam -est -nr 2 -noinit
+
+
+
+
+
---
+++ tmb_examples/samtmb.R
@@ -0,0 +1,27 @@
+source("samtmb_data.R")
+
+library(TMB)
+compile("samtmb.cpp")
+dyn.load("samtmb.so")
+
+parameters <- list(
+  logFpar=c(-11, -10, -10, -9, -9),
+  logQpow=numeric(0),
+  logSdLogFsta=rep(-0.693147,max(data$keyVarF)+1),
+  logSdLogN=c(0.356675, -0.356675),
+  logSdLogObs=c(-0.356675, -0.356675, -0.356675, -0.356675, -0.356675),
+  rec_loga=1,
+  rec_logb=-12,
+  logit_rho=0.55, 
+  logScale=numeric(data$noScaledYears),  
+  logScaleSSB=if(any(data$fleetTypes%in%c(3,4))){numeric(1)}else{numeric(0)},
+  logPowSSB=if(any(data$fleetTypes==4)){numeric(1)}else{numeric(0)},
+  logSdSSB=if(any(data$fleetTypes%in%c(3,4))){numeric(1)}else{numeric(0)},
+  logF=matrix(0, nrow=max(data$keyLogFsta)+1,ncol=data$noYears),
+  logN=matrix(0, nrow=data$maxAge-data$minAge+1, ncol=data$noYears)
+)
+
+obj <- MakeADFun(data,parameters,random=c("logN","logF"),DLL="samtmb")
+system.time(opt<-nlminb(obj$par,obj$fn,obj$gr))
+rep<-sdreport(obj)
+rep
