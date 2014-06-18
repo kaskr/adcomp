9da64fb71a6d909bd78ce023f047d6c360ed4c7f
Théo <theo.michelot@insa-rouen.fr>
2014-06-18 12:41:54 +0200

    Documentation for the file

--- TMB/inst/include/Vectorize.hpp
+++ TMB/inst/include/Vectorize.hpp
@@ -1,5 +1,7 @@
+/** 	\file
+	\brief Macros to do vectorization.
+	*/
 
-/* Macros to do vectorization */
 #define VECTORIZE1(FUN)				\
 template <class Type>				\
 vector<Type> FUN(const vector<Type> &x)		\
