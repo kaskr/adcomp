a16ac9418c8d96ac8e22ee9017803880bbaff098
Théo <theo.michelot@insa-rouen.fr>
2014-06-24 16:16:27 +0200

    Remove dtweedie and pnorm functions.

--- TMB/inst/include/distributions_R.hpp
+++ TMB/inst/include/distributions_R.hpp
@@ -192,39 +192,6 @@ Type dlogis(Type x, Type location, Type scale, int give_log)
 // Vectorize dlogis
 VECTORIZE4_ttti(dlogis);
 
-/**	@name Normal distribution.
-	Functions relative to the normal distribution.
-	*/
-/**@{*/
-/**	\brief Cumulative distribution function of the standard normal distribution.
-	\ingroup R_style_distribution
-	\param give_log 1 if one wants the log-probability, 0 otherwise.
-	*/
-template<class Type>
-Type pnorm_standard(Type x, int give_log)
-{
-	return Type(0.5)+0.5*erf(x/sqrt(2));
-}
-
-/**	\brief Cumulative distribution function of the normal distribution.
-	\ingroup R_style_distribution
-	\param mean Mean of the normal distribution.
-	\param sd Standard deviation of the normal distribution.
-	\param give_log 1 if one wants the log-probability, 0 otherwise.
-	*/
-template<class Type>
-Type pnorm(Type x, Type mean, Type sd, int give_log)
-{
-	Type p = (x-mean)/sd;
-
-	Type tmp = CppAD::CondExpLt(x,mean,Type(0),Type(1));
-	return CppAD::CondExpLe(sd,Type(0),tmp,pnorm_standard(p,give_log));
-}
-
-// Vectorize pnorm
-VECTORIZE4_ttti(pnorm);
-/**@}*/
-
 /**	\brief Probability density function of the skew-normal distribution.
 	\ingroup R_style_distribution
 	\param alpha Slant parameter.
@@ -252,68 +219,4 @@ Type dt(Type x, Type df, int give_log)
 	if(!give_log) return exp(logres);
 	else return logres;
 }
-
-/**	@name Tweedie distribution.
-	Functions relative to the Tweedie distribution.
-	*/
-/**@{*/
-/**	\brief Deviance function for the Tweedie distribution.
-	\ingroup R_style_distribution
-	\param mu Mean.
-	\param power Value of p such that the variance is \f$ var[Y] = \phi*\mu^p \f$.
-	*/
-template <class Type>
-Type tweedie_dev(Type y, Type mu, Type power)
-{
-	Type p = power;
-	Type res;
-	Type dev0, dev1, dev2, dev3;
-	
-	dev0 = (y-mu)*(y-mu)/2;
-	dev1 = CppAD::CondExpEq(y,Type(0),mu,y*log(y/mu)-(y-mu));
-	dev2 = log(mu/y)+y/mu-1;
-	dev3 = pow(y,2-p)/((1-p)*(2-p)) - (y*pow(mu,1-p))/(1-p) + pow(mu,2-p)/(2-p);
-	res = dev3;
-
-	res = CppAD::CondExpEq(p,Type(0),dev0,res);
-	res = CppAD::CondExpEq(p,Type(1),dev1,res);
-	res = CppAD::CondExpEq(p,Type(2),dev2,res);
-	return 2*res;
-}
-
-/**	\brief Saddlepoint density for the Tweedie distribution.
-	\ingroup R_style_distribution
- 	\param xi Value of p such that the variance is \f$ var[Y] = \phi*\mu^p \f$.
-	\param mu Mean.
-	\param phi Dispersion.
-	\param eps Offset in computing the variance function. Default value : 1/6.
-	\param give_log 1 if one wants the log-probability, 0 otherwise (default).
-	*/	
-template <class Type>
-Type dtweedie_saddle(Type y, Type xi, Type mu, Type phi, Type eps=Type(1)/6, int give_log=0)
-{
-	Type p = xi;
-	Type y_eps = y;
-	Type dev;
-	Type density0, density;
-	
-		
-	y_eps = CppAD::CondExpLt(p,Type(2),y+eps,y_eps);
-	
-	dev = tweedie_dev(y,mu,p);
-	density = 1/sqrt(2*M_PI*phi*pow(y_eps,p))*exp(-dev/(2*phi));
-
-	density0 = CppAD::CondExpLt(p,Type(2), exp(-pow(mu,2-p)/(phi*(2-p))) ,Type(0));
-	density0 = CppAD::CondExpLt(p,Type(1),Type(0),density0);
-	
-	density = CppAD::CondExpEq(y,Type(0),density0,density);
-	if(!give_log) return density;
-	else return log(density);
-}
-
-// Vectorize dtweedie_saddle
-VECTORIZE6_Ttttti(dtweedie_saddle);
-
-
-/**@}*/
 
