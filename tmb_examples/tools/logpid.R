3d3631282a599561aeffff07c7db7e42a96f2c57
Kasper Kristensen <kaskr@imm.dtu.dk>
2015-03-02 21:16:44 +0100

    Squashed commit of the following:     - Separate folder for 'tools' and new logpid script.     - Add 'logpid' to Makefile

---
+++ tmb_examples/tools/logpid.R
@@ -0,0 +1,18 @@
+## Log cpu and memory usage of test example
+logpid <- function(){
+    script <-
+"logpid() {
+  while sleep .1;
+    do env -i ps -p $1 -o etime= -o pcpu= -o pmem= ;
+  done;
+};
+echo \"    elapsed pcpu pmem\" > FILE;
+logpid PID >> FILE &\n"
+    script <- gsub("PID", Sys.getpid(), script)
+    script <- gsub("FILE", paste0(Sys.getenv("example"), ".logpid") , script)
+    cat(script)
+    system(script)
+}
+logpid()
+example <- Sys.getenv("example")
+source(paste0(example,".R"))
