3694c6168c65b7c5ccbdb05acc7cea38d55ff87b
Kasper Kristensen <kaskr@imm.dtu.dk>
2015-08-10 16:06:18 +0200

    oneStepPredict: Didn't work with maps #108

--- TMB/R/validation.R
+++ TMB/R/validation.R
@@ -181,9 +181,8 @@ oneStepPredict <- function(obj,
     tmp[-obj$env$random] <- TRUE
     li <- lapply(obj$env$parList(par = tmp), function(x) any(x!=0))
     fix <- names(li)[unlist(li)]
-    parameters <- obj$env$parameters
-    ## Overwrite map
-    args$map <- lapply(parameters[fix], function(x)factor(x*NA))
+    map <- lapply(args$parameters[fix], function(x)factor(x*NA))
+    args$map <- map ## Overwrite map
     ## Find randomeffects character
     args$random <- names(li[!unlist(li)])
     ## Move data$name to parameter$name
