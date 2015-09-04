e937e2c571a142bc7a68bd2c786b2df66967190d
Arni Magnusson <arnima@hafro.is>
2015-09-04 17:00:09 +0000

    emacs tmb-mode 1.2

--- tmb_syntax/tmb.el
+++ tmb_syntax/tmb.el
@@ -5,7 +5,7 @@
 ;; Author:   Arni Magnusson
 ;; Keywords: languages
 
-(defconst tmb-mode-version "1.1" "TMB Mode version number.")
+(defconst tmb-mode-version "1.2" "TMB Mode version number.")
 
 ;; This file is not part of GNU Emacs.
 
@@ -25,8 +25,8 @@
 ;;; Commentary:
 ;;
 ;; Major mode for editing Template Model Builder (TMB) code, derived from
-;; `c++-mode'. Provides syntax highlighting. The syntax groups for highlighting
-;; are:
+;; `c++-mode'. Provides syntax highlighting, IDE compilation, file manipulation,
+;; and smaller tools. The syntax groups for highlighting are:
 ;;
 ;; Face                          Example
 ;; tmb-data-face                 DATA_VECTOR
@@ -38,6 +38,9 @@
 ;; font-lock-function-name-face  [Type] myfunction
 ;; font-lock-keyword-face        log
 ;; font-lock-type-face           Type
+;;
+;; Keybindings defined in `tmb-mode' are overridden by user `c-mode-common-hook'
+;; and `c++-mode-hook', which can then be overridden by user `tmb-mode-hook'.
 ;;
 ;; Installation:
 ;;
@@ -51,13 +54,16 @@
 ;;
 ;; Customization:
 ;;
-;; If you want to set syntax colors, here is an example that does that:
+;; If you want to set the default R command, syntax colors, or keybindings, here
+;; is an example that does that:
 ;; (defun my-tmb-hook ()
+;;   (setq tmb-r-command "Rscript")
 ;;   (set-face-attribute 'tmb-data-face      nil :foreground "dodgerblue")
 ;;   (set-face-attribute 'tmb-parameter-face nil :foreground "dodgerblue")
 ;;   (set-face-attribute 'tmb-report-face    nil :foreground "dodgerblue")
 ;;   (set-face-attribute 'font-lock-variable-name-face nil
-;;                       :foreground 'unspecified))
+;;                       :foreground 'unspecified)
+;;   (local-set-key [?\C-c ?\C-c] 'tmb-run-r-script))
 ;; (add-hook 'tmb-mode-hook 'my-tmb-hook)
 ;;
 ;; Usage:
@@ -71,36 +77,47 @@
 
 ;;; History:
 ;;
-;; 01 Sep 2015  1.0  Created main function `tmb-mode', derived from `c++-mode'.
+;; 04 Sep 2015  1.2  Added user functions `tmb-clean', `tmb-for',
+;;                   `tmb-kill-process', `tmb-open', `tmb-run-another',
+;;                   `tmb-run-r-script', and `tmb-toggle-section'. Added user
+;;                   variable `tmb-r-command'. Disabled `abbrev-mode'.
 ;; 03 Sep 2015  1.1  Shortened list of recognized FUNCTIONS for maintainability.
+;; 01 Sep 2015  1.0  Created main function `tmb-mode', derived from `c++-mode'.
 
 ;;; Code:
 
 ;; 1  Preamble
 
 (require 'cc-mode)
-(defgroup tmb nil "Major mode for editing Template Model Builder code."
+(defgroup tmb nil
+  "Major mode for editing Template Model Builder code."
   :tag "TMB" :group 'languages)
 ;; Switch to `tmb-mode' if C++ buffer includes TMB header
 (setq magic-mode-regexp-match-limit 40000)
-(defun tmb-include-p () "Search for #include <TMB.hpp> in C++ file."
-       (if (string-equal (file-name-extension (buffer-name)) "cpp")
-           (re-search-forward "#include ?<TMB.hpp>"
-                              magic-mode-regexp-match-limit t)))
+(defun tmb-include-p ()
+  "Search for #include <TMB.hpp> in C++ file."
+  (if (string-equal (file-name-extension (buffer-name)) "cpp")
+      (re-search-forward "#include ?<TMB.hpp>"
+                         magic-mode-regexp-match-limit t)))
 (setq magic-mode-alist '((tmb-include-p . tmb-mode)))
 
 ;; 2  User variables
 
+(defcustom tmb-r-command "R --quiet --vanilla <"
+  "Shell command to run R script using `tmb-run-r-script'."
+  :tag "R" :type 'string :group 'tmb)
 (defface tmb-data-face '((t :inherit font-lock-type-face))
   "Font Lock face to highlight TMB data macros." :group 'tmb)
-(defvar tmb-data-face 'tmb-data-face "Face name for TMB data macros.")
+(defvar tmb-data-face 'tmb-data-face
+  "Face name for TMB data macros.")
 (defface tmb-parameter-face '((t :inherit font-lock-type-face))
   "Font Lock face to highlight TMB parameter macros." :group 'tmb)
 (defvar tmb-parameter-face 'tmb-parameter-face
   "Face name for TMB parameter macros.")
 (defface tmb-report-face '((t :inherit font-lock-keyword-face))
   "Font Lock face to highlight TMB report macros." :group 'tmb)
-(defvar tmb-report-face 'tmb-report-face "Face name for TMB report macros.")
+(defvar tmb-report-face 'tmb-report-face
+  "Face name for TMB report macros.")
 
 ;; 3  Internal variables
 
@@ -149,7 +166,7 @@
              "dgamma" "dlgamma" "pgamma" "qgamma"
              "dweibull" "pweibull" "qweibull"))
           (CONSTANTS '("dim"))
-          (IMPORTANT '("error")))
+          (WARNINGS '("error")))
       (list
        (cons (regexp-opt TYPE 'words) font-lock-type-face)
        (cons (regexp-opt DATA 'words) 'tmb-data-face)
@@ -157,16 +174,54 @@
        (cons (regexp-opt REPORT 'words) 'tmb-report-face)
        (cons (regexp-opt FUNCTIONS 'words) font-lock-keyword-face)
        (cons (regexp-opt CONSTANTS 'words) font-lock-constant-face)
-       (cons (regexp-opt IMPORTANT 'words) font-lock-builtin-face)))))
+       (cons (regexp-opt WARNINGS 'words) font-lock-warning-face)))))
 (nconc tmb-font-lock-keywords c++-font-lock-keywords)
 
 ;; 4  User functions
 
-(defun tmb-help () "Show help page for `tmb-mode'." (interactive)
-       (describe-function 'tmb-mode)(switch-to-buffer "*Help*")
-       (delete-other-windows)(message nil))
-(defun tmb-mode-version () "Show TMB Mode version number." (interactive)
-       (message (concat "TMB Mode version " tmb-mode-version)))
+(defun tmb-clean ()
+  "Remove C++ binary files (*.o *.so *.dll)." (interactive)
+  (let* ((prog (file-name-sans-extension (buffer-name)))
+         (pattern (concat prog "\\.o\\|" prog "\\.so\\|" prog "\\.dll"))
+         (files (directory-files "." nil pattern t)))
+    (dolist (x files)(delete-file x)))(message "Removed binary files."))
+(defun tmb-for ()
+  "Insert for(int i=0; i<; i++)." (interactive)
+  (insert "for(int i=0; i<; i++)")(search-backward ";"))
+(defun tmb-help ()
+  "Show help page for `tmb-mode'." (interactive)
+  (describe-function 'tmb-mode)(switch-to-buffer "*Help*")(delete-other-windows)
+  (message nil))
+(defun tmb-kill-process ()
+  "Stop the current process, usually TMB compilation or model run."
+  (interactive)(kill-process (car (process-list))))
+(defun tmb-mode-version ()
+  "Show TMB Mode version number." (interactive)
+  (message (concat "TMB Mode version " tmb-mode-version)))
+(defun tmb-open (ext)
+  "Open file with extension EXT in other window." (interactive "sExtension: ")
+  (let ((file (concat (file-name-sans-extension (buffer-name)) "." ext)))
+    (if (not (file-regular-p file))(error "File %s not found" file)
+      (find-file-other-window file))))
+(defun tmb-run-another (script)
+  "Run another R script, querying user for SCRIPT filename.\n
+If script has same filename prefix as current buffer, then use
+`tmb-run-r-script' instead.\n
+Filename history is accessible using M-p and up arrow."
+  (interactive "fRun R script: ")(save-buffer)
+  (compile (concat tmb-r-command " " script))
+  (with-current-buffer "*compilation*" (setq show-trailing-whitespace nil)))
+(defun tmb-run-r-script ()
+  "Run R script with same filename prefix as current buffer.\n
+If script has a different filename, then use `tmb-run-another' instead."
+  (interactive)
+  (let ((prefix (file-name-sans-extension (buffer-name))))
+    (save-buffer)(compile (concat tmb-r-command " " prefix ".R")))
+  (with-current-buffer "*compilation*" (setq show-trailing-whitespace nil)))
+(defun tmb-toggle-section ()
+  "Toggle whether the current section is indicated in the mode line."
+  (interactive)(which-function-mode (if which-function-mode 0 1))
+  (message "Section indicator %s" (if which-function-mode "ON" "OFF")))
 
 ;; 5  Main function
 
@@ -176,7 +231,16 @@
 The `tmb-help' command shows this page.\n
 \\{tmb-mode-map}"
   (set (make-local-variable 'font-lock-defaults)
-       '(tmb-font-lock-keywords nil nil)))
+       '(tmb-font-lock-keywords nil nil))
+  (abbrev-mode 0))
 (modify-syntax-entry ?_ "w" tmb-mode-syntax-table)
+(define-key tmb-mode-map [?\C-c ?\C-/] 'tmb-help          )
+(define-key tmb-mode-map [?\C-c ?\C-a] 'tmb-run-another   )
+(define-key tmb-mode-map [?\C-c ?\C-d] 'tmb-clean         )
+(define-key tmb-mode-map [?\C-c ?\C-f] 'tmb-for           )
+(define-key tmb-mode-map [?\C-c ?\C-k] 'tmb-kill-process  )
+(define-key tmb-mode-map [?\C-c ?\C-o] 'tmb-open          )
+(define-key tmb-mode-map [?\C-c ?\C-r] 'tmb-run-r-script  )
+(define-key tmb-mode-map [?\C-c ?\C-s] 'tmb-toggle-section)
 
 (provide 'tmb)
