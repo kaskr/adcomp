37909a60606f1781626c01bc37ae553f09b5dce3
colemonnahan <monnahc@uw.edu>
2015-11-12 12:14:20 -0800

    Update test2d to track adapted eps values

--- mcmc_test/test2d.R
+++ mcmc_test/test2d.R
@@ -15,12 +15,13 @@ plotSurface <- function(model){
 ## Get independent sample
 getOneSample <- function(method, plot = FALSE){
     sim <- switch(method,
-                  "HMC"  = mcmc(model, 1000,  method, L=5, eps=.5),
-                  "NUTS" = mcmc(model, 100,   method, eps=.5),
-                  "RWM"  = mcmc(model, 10000, method) )
-    ans <- tail(sim, 1)
+                  "HMC"  = mcmc(model, 1000,  method, L=5, diagnostic=TRUE),
+                  "NUTS" = mcmc(model, 100,   method, diagnostic=TRUE),
+                  "RWM"  = mcmc(model, 10000, method, diagnostic=TRUE) )
+    ans <- tail(sim$par,1)
     if(plot) points(ans)
-    unlist(ans)
+    eps <- if(method=="RWM") 0 else tail(sim$epsbar,1)
+    data.frame(ans, eps=eps)
 }
 
 ## Simulate independent draws from target:
@@ -28,17 +29,18 @@ set.seed(123)
 nsim <- 100
 
 ## Tests all the mcmc algorithms
-ans <- list()
+eps <- ans <- list()
 for(example in c(0, 1)){
+    dev.new(); par(mfrow=c(3,2))
     parameters <- list(x=0, y=0)
     model <- MakeADFun(list(example=example), parameters)
     for(method in c("HMC", "NUTS", "RWM")){
-        dev.new(); layout(t(1:2))
         plotSurface(model)
-        system.time( sim <- replicate(nsim, getOneSample(method, TRUE)) )
+        system.time( sim <- lapply(1:nsim, function(x) getOneSample(method, TRUE)) )
         ## Transform the draws to iid uniform variates and test for
         ## correctness:
-        uv <- apply(sim, 2, function(xy) model$report(xy)$uv)
+        sim <- do.call(rbind, sim)
+        uv <- apply(sim[,1:2], 1, function(xy) model$report(xy)$uv)
         plot(seq(0, 1, length = 2*nsim), sort(uv)); abline(0,1)
         title(paste0(method,example))
         ans[[paste0(method,example)]] <- list(
@@ -46,5 +48,11 @@ for(example in c(0, 1)){
             cor(t(uv)),
             ks.test(uv, "punif")
             )
+        eps[[paste0(method,example)]] <- sim$eps
     }
 }
+dev.new()
+## Make sure the adaptive eps algorithms are converging consistently
+eps <- data.frame(do.call(cbind, eps))[,c(-3,-6)]
+boxplot(eps)
+apply(eps, 2, function(x) sum(!is.finite(x)))
