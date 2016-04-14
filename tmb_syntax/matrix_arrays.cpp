8e708e4686c06322cdbbfd72611f23a850d74052
skaug <hskaug@gmail.com>
2016-04-14 17:53:01 +0200

    Update matrix and array examples

--- tmb_syntax/matrix_arrays.cpp
+++ tmb_syntax/matrix_arrays.cpp
@@ -5,55 +5,52 @@ template<class Type>
 Type objective_function<Type>::operator() ()
 {
   // Data objects
-  DATA_INTEGER(i);		// Scalar integer
+  DATA_VECTOR(v1);		
+  DATA_MATRIX(m1);		
+  DATA_ARRAY(a1);		
 
   // Parameter objects
-  PARAMETER(p)
-
-  // Objects of double type so that they can be printed
-  vector<double> v1(2); 
-  v1 << 9,11;
-  std::cout << "v1 = \n" 	<< v1 				<< "\n";
-  matrix<double> m1(2,2); 
-  m1 << 1,2,3,4;
-  std::cout << "m1 = \n" 	<< m1 				<< "\n";
-  array<double> a1(2,2); 
-  a1 << 11,12,13,14;
-  std::cout << "a1 = \n" 	<< a1 				<< "\n";
+  PARAMETER(p)		// Not used in this example
 
   // Obtaining dimensions of objects
-  vector<int> d1 = a1.dim;		
-  std::cout << "Dimension of array a1= \n" 	<< d1 				<< "\n";
+  REPORT(a1.dim);
   vector<int> d2(2);			
   d2(0) = m1.rows();
   d2(1) = m1.cols();
-  std::cout << "Dimension of matrix m1= \n" 	<< d2 				<< "\n";
+  REPORT(d2);
   int d3 = v1.size();			
-  std::cout << "Dimension of vector v1= " 	<< d3 				<< "\n";
+  REPORT(d3);
+  
 
-  // Matrix multiplication versus elementwise operations
-  std::cout << "Element-wise product of arrays = \n"	<< a1*a1 				<< "\n";
-  std::cout << "Element-wise division of arrays = \n" 	<< a1/a1 				<< "\n";
-  std::cout << "Element-wise product of matrices = \n"	<< m1.array()*m1.array() 		<< "\n";
-  std::cout << "matrix product = \n" 			<< m1*m1 				<< "\n";
-  std::cout << "matrix-vector product = \n" 		<< m1*v1 				<< "\n";
-  std::cout << "Element-wise product of vectors = \n" 	<< v1*v1 				<< "\n";
-  std::cout << "Element-wise division of vectors = \n" 	<< v1/v1 				<< "\n";
-  std::cout << "Inner product of vectors = " 		<< (v1*v1).sum()			<< "\n";
+  // Matrix multiplication versus elementwise multiplication (similar for addition, subtraction,...)
+  matrix<Type> m1m1 = m1*m1;	// Matrix multiplication of matrices
+  REPORT(m1m1);  
+  array<Type> a1a1 = a1*a1;	    // Element-wise multiplication of arrays 
+  REPORT(a1a1);  
+  matrix<Type> m1m1_by_element = (m1.array()*m1.array()).matrix();	// Elementwise multiplication of matrices
+  REPORT(m1m1_by_element);  
+  array<Type> a1a1_matrix_mult(2,2);
+  a1a1_matrix_mult = (a1.matrix()*a1.matrix()).array();	// Matrix multiplication of arrays
+  REPORT(a1a1_matrix_mult);  
 
+  // Matrix-vector multiplication
+  REPORT(m1*v1);	// matrix-vector product (linear algebra style)
+  Type v1_norm2 = (v1*v1).sum();	// Inner product v1*v1
+  REPORT(v1_norm2);
+  
   // Indexing objects
-  std::cout << "Element (1,1) of matrix m1= " 		<< m1(1,1) 				<< "\n";
-  std::cout << "2nd row of matrix m1= \n" 		<< m1.row(1) 				<< "\n";
-  std::cout << "2nd col of matrix m1= \n" 		<< m1.col(1) 				<< "\n";
-  std::cout << "Element (1,1) of array a1= " 		<< a1(1,1) 				<< "\n";
-  std::cout << "2nd row of array a1= \n"		<< a1.transpose().col(1)		<< "\n";
-  std::cout << "2nd col of array a1= \n" 		<< a1.col(1) 				<< "\n";
-  std::cout << "2nd element of vector v1= "		<< v1(1) 				<< "\n";
+  m1(1,1); 		// Element (1,1) of matrix m1
+  m1.row(1);    // 2nd row of matrix m1
+  m1.col(1); 	// 2nd col of matrix m1
+  a1(1,1);		// Element (1,1) of array a1
+  a1.transpose().col(1); // 2nd row of array a1
+  a1.col(1); 	//2nd col of array a1
+  v1(1);		// 2nd element of vector v1
 
   // Subsetting matrices and vectors
-  std::cout << "First element of v1= "			<< v1.head(1) 				<< "\n";
-  std::cout << "Last element of v1= "			<< v1.tail(1) 				<< "\n";
-  std::cout << "Block of m1 consisting of m1(1,1)= "	<< m1.block(0,0,1,1)			<< "\n";
+  v1.head(1);		// First element of v1
+  v1.tail(1); 		// Last element of v1
+  m1.block(0,0,1,1);	// Block of m1 consisting of m1(1,1)
 
   // Subsetting arrays
   // See R help pages for "template", i.e. "help(template)" in R  
@@ -64,7 +61,6 @@ Type objective_function<Type>::operator() ()
   m1.diagonal(); 
   m1.asDiagonal();
   
-
 
   Type ans;
   return ans;
