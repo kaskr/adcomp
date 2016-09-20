d076d456c1c72ed800ed3fa474623b679764d06a
Kasper Kristensen <kaskr@dtu.dk>
2016-09-20 11:46:31 +0200

    Add onLoad check on 'Matrix' package version

--- TMB/R/zzz.R
+++ TMB/R/zzz.R
@@ -5,8 +5,33 @@
 ##   library.dynam("TMB", pkg, lib)
 ## }
 
+checkMatrixPackageVersion <- function() {
+    ## It is unsafe to use the TMB package with versions of 'Matrix'
+    ## other than the one TMB was originally built with.
+    file <- paste0(system.file(package="TMB"),"/Matrix-version")
+    cur.Matrix.version <- as.character(packageVersion("Matrix"))
+    if(!file.exists(file)) {
+        writeLines(cur.Matrix.version, con = file)
+    }
+    TMB.Matrix.version <- readLines(file)
+    if(!identical(TMB.Matrix.version, cur.Matrix.version)) {
+        warning(
+            "Package version inconsistency detected.\n",
+            "TMB was built with Matrix version ",
+            TMB.Matrix.version,
+            "\n",
+            "Current Matrix version is ",
+            cur.Matrix.version,
+            "\n",
+            "Please re-install 'TMB' from source ",
+            "or restore original 'Matrix' package"
+        )
+    }
+}
+
 .onLoad <- function(lib, pkg) {
-  library.dynam("TMB", pkg, lib)
+    library.dynam("TMB", pkg, lib)
+    checkMatrixPackageVersion()
 }
 
 ## .LastLib <- function(libpath)
