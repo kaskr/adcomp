df15b3ab8b46a8ae4b2475236e3525b3057d4f7d
colemonnahan <monnahc@uw.edu>
2016-10-13 15:45:02 -0700

    Add simple mvn model for mcmc testing

---
+++ mcmc_test/test_mvn.R
@@ -0,0 +1,26 @@
+## Use a simple multivariate normal for testing MCMC functionality
+
+library(shinystan)
+library(TMB)
+compile('mvn.cpp')
+dyn.load(dynlib('mvn'))
+
+#devtools::install('C:/Users/Cole/shinystan-feature-issue-136')
+
+covar <- matrix(c(1,-.9, -.9,1), nrow=2)
+obj <- MakeADFun(data=list(d=2, covar=covar), parameters=list(X=c(1,-1)))
+
+hmc <- run_mcmc(obj=obj, nsim=1000, algorithm='HMC', chains=2, L=5,
+                covar=covar)
+str(hmc)
+sso <- as.shinystan(hmc$samples, burnin=500,
+                    sampler_params=hmc$sampler_params, algorithm='HMC')
+launch_shinystan(sso)
+
+nuts <- run_mcmc(obj=obj, nsim=1000, algorithm='NUTS', chains=2,
+                covar=NULL)
+str(nuts)
+sso <- as.shinystan(nuts$samples, burnin=500, max_treedepth=8,
+                    sampler_params=nuts$sampler_params, algorithm='NUTS')
+launch_shinystan(sso)
+
