1747cbdd479b8db3ad501fcdc73a7294a96f3ed3
Kasper Kristensen <kaskr@dtu.dk>
2016-11-13 10:47:20 +0100

    Add script to check simulations

---
+++ tmb_syntax/check_simulations.R
@@ -0,0 +1,86 @@
+
+require(TMB)
+compile('check_simulations.cpp')
+dyn.load(dynlib('check_simulations'))
+set.seed(1)
+
+.results <- list()
+checkSimulation <- function(obj) {
+    g <- obj$gr(obj$par) ## Gradient at true parameter
+    H <- obj$he(obj$par) ## Hessian at true parameter
+    stopifnot(all( eigen(H)$value > 0 ))
+    ## g ~ N(0, H)
+    chi.sq <- g %*% solve(H) %*% t(g)
+    p.value <- 1 - pchisq(chi.sq, df=length(obj$par))
+    ans <- data.frame(distribution=obj$env$data$distr,
+                      p.value=p.value, PASSED = p.value>.05 )
+    .GlobalEnv$.results <- rbind(.GlobalEnv$.results, ans)
+    ans
+}
+
+n <- 100000
+
+######################################################################
+
+data <- list(distr="norm", n=n)
+parameters <- list(mu=0, sd=1)
+obj <- MakeADFun(data, parameters)
+checkSimulation(obj)
+
+######################################################################
+
+data <- list(distr="gamma", n=n)
+parameters <- list(shape=3, scale=2)
+obj <- MakeADFun(data, parameters)
+checkSimulation(obj)
+
+######################################################################
+
+data <- list(distr="pois", n=n)
+parameters <- list(lambda=5)
+obj <- MakeADFun(data, parameters)
+checkSimulation(obj)
+
+######################################################################
+
+data <- list(distr="nbinom", n=n)
+parameters <- list(size=5, prob=.1)
+obj <- MakeADFun(data, parameters)
+checkSimulation(obj)
+
+######################################################################
+
+data <- list(distr="nbinom2", n=n)
+parameters <- list(mu=5, var=10)
+obj <- MakeADFun(data, parameters)
+checkSimulation(obj)
+
+######################################################################
+data <- list(distr="AR1", n=n)
+parameters <- list(phi = .2)
+obj <- MakeADFun(data, parameters)
+checkSimulation(obj)
+
+######################################################################
+
+data <- list(distr="ARk", n=n)
+parameters <- list(phi = c(.9, .05))
+obj <- MakeADFun(data, parameters)
+checkSimulation(obj)
+
+######################################################################
+
+data <- list(distr="MVNORM", n=1000)
+parameters <- list(phi = c(.9))
+obj <- MakeADFun(data, parameters)
+checkSimulation(obj)
+
+######################################################################
+data <- list(distr="SEPARABLE", n=n)
+parameters <- list(phi1 = .2, phi2 = c(.9, .05))
+obj <- MakeADFun(data, parameters)
+checkSimulation(obj)
+
+######################################################################
+
+cat("\n"); print(.results)
---
+++ tmb_syntax/check_simulations.cpp
@@ -0,0 +1,72 @@
+#include <TMB.hpp>
+
+template<class Type>
+Type objective_function<Type>::operator() ()
+{
+  DATA_STRING(distr);
+  DATA_INTEGER(n);
+  Type ans = 0;
+
+  if (distr == "norm") {
+    PARAMETER(mu);
+    PARAMETER(sd);
+    vector<Type> x = rnorm(n, mu, sd);
+    ans -= dnorm(x, mu, sd, true).sum();
+  }
+  else if (distr == "gamma") {
+    PARAMETER(shape);
+    PARAMETER(scale);
+    vector<Type> x = rgamma(n, shape, scale);
+    ans -= dgamma(x, shape, scale, true).sum();
+  }
+  else if (distr == "pois") {
+    PARAMETER(lambda);
+    vector<Type> x = rpois(n, lambda);
+    ans -= dpois(x, lambda, true).sum();
+  }
+  else if (distr == "nbinom") {
+    PARAMETER(size);
+    PARAMETER(prob);
+    vector<Type> x = rnbinom(n, size, prob);
+    ans -= dnbinom(x, size, prob, true).sum();
+  }
+  else if (distr == "nbinom2") {
+    PARAMETER(mu);
+    PARAMETER(var);
+    vector<Type> x = rnbinom2(n, mu, var);
+    ans -= dnbinom2(x, mu, var, true).sum();
+  }
+  else if (distr == "AR1") {
+    PARAMETER(phi);
+    vector<Type> x(n);
+    density::AR1(phi).simulate(x);
+    ans += density::AR1(phi)(x);
+  }
+  else if (distr == "ARk") {
+    PARAMETER_VECTOR(phi);
+    vector<Type> x(n);
+    density::ARk(phi).simulate(x);
+    ans += density::ARk(phi)(x);
+  }
+  else if (distr == "MVNORM") {
+    PARAMETER(phi);
+    matrix<Type> Sigma(5,5);
+    for(int i=0; i<Sigma.rows(); i++)
+      for(int j=0; j<Sigma.rows(); j++)
+        Sigma(i,j) = exp( -phi * abs(i - j) );
+    density::MVNORM_t<Type> nldens = density::MVNORM(Sigma);
+    for(int i = 0; i<n; i++) {
+      vector<Type> x = nldens.simulate();
+      ans += nldens(x);
+    }
+  }
+  else if (distr == "SEPARABLE") {
+    PARAMETER(phi1);
+    PARAMETER_VECTOR(phi2);
+    array<Type> x(100, 200);
+    SEPARABLE( density::ARk(phi2), density::AR1(phi1) ).simulate(x);
+    ans += SEPARABLE( density::ARk(phi2), density::AR1(phi1) )(x);
+  }
+  else error( ("Invalid distribution '" + distr + "'").c_str() );
+  return ans;
+}
