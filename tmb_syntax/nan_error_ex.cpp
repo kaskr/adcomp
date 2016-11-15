a96d4ce89432747154399fae4904400604f2689a
Kasper Kristensen <kaskr@dtu.dk>
2016-11-15 10:20:37 +0100

    doxygen: - Comment on zero-initialize - Formatting of nan error example - Add generic simulation study - Add 'unsupported' to excludes

--- tmb_syntax/nan_error_ex.cpp
+++ tmb_syntax/nan_error_ex.cpp
@@ -5,15 +5,14 @@
 template<class Type>
 Type objective_function<Type>::operator() ()
 {
-  	feenableexcept(FE_INVALID | FE_OVERFLOW | FE_DIVBYZERO | FE_UNDERFLOW); // Extra line needed
+  feenableexcept(FE_INVALID | FE_OVERFLOW | FE_DIVBYZERO | FE_UNDERFLOW); // Extra line needed
 	
-        DATA_SCALAR(lambda);
-        PARAMETER(x);
-        Type f;
-	
-        f = sqrt(-1.); // FE_INVALID (sqrt(-1.) returns NaN)
-        //f = 1./0.; // FE_DIVBYZERO (division by zero)
-        //f = exp(100000.); // FE_OVERFLOW (exp(100000.) returns Inf) 		[Does not work on all platforms]
-  	//f = exp(-100000.); // FE_UNDERFLOW (exp(-100000.) returns 0)
-  	return f;
+  DATA_SCALAR(lambda);
+  PARAMETER(x);
+  Type f;
+  f = sqrt(-1.);        // FE_INVALID   ( sqrt(-1.) returns NaN )
+  //f = 1./0.;          // FE_DIVBYZERO ( division by zero )
+  //f = exp(100000.);   // FE_OVERFLOW  ( exp(100000.) returns Inf )   [Does not work on all platforms]
+  //f = exp(-100000.);  // FE_UNDERFLOW ( exp(-100000.) returns 0 )
+  return f;
 }
