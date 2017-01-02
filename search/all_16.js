f80267113fd2a89e8ff94ebe097b0f85ce0a5454
Travis CI <kaskr@dtu.dk>
2017-01-02 21:31:10 +0000

    Deploy to GitHub Pages: d4e42fe0fcf47fc2e1bb8d8abe1cb3fb82c4953c

--- search/all_16.js
+++ search/all_16.js
@@ -2,8 +2,8 @@ var searchData=
 [
   ['vec',['vec',['../structtmbutils_1_1matrix.html#a5479cff1a0b3231b1c4270c07759e83e',1,'tmbutils::matrix::vec()'],['../structmatrix.html#aff2bcb64bc91137b9635369a6cee82b6',1,'matrix::vec()']]],
   ['vecscale',['VECSCALE',['../density_8hpp.html#aae30a0275dafde20092e4c12c6bda61e',1,'VECSCALE():&#160;density.hpp'],['../namespacedensity.html#ad4883b13b683e6b9e1e27846b74e4680',1,'density::VECSCALE()']]],
-  ['vecscale_5ft',['VECSCALE_t',['../classdensity_1_1VECSCALE__t.html',1,'density']]],
   ['vecscale_5ft',['VECSCALE_t',['../classVECSCALE__t.html',1,'']]],
+  ['vecscale_5ft',['VECSCALE_t',['../classdensity_1_1VECSCALE__t.html',1,'density']]],
   ['vector',['vector',['../structtmbutils_1_1vector.html',1,'tmbutils']]],
   ['vector',['vector',['../structvector.html',1,'']]],
   ['vector_2ehpp',['vector.hpp',['../vector_8hpp.html',1,'']]],
@@ -11,7 +11,7 @@ var searchData=
   ['vector_3c_20const_20char_20_2a_3e',['vector&lt; const char *&gt;',['../structvector.html',1,'']]],
   ['vector_3c_20density_3a_3amvnorm_5ft_3c_20scalartype_20_3e_20_3e',['vector&lt; density::MVNORM_t&lt; scalartype &gt; &gt;',['../structtmbutils_1_1vector.html',1,'tmbutils']]],
   ['vector_3c_20int_20_3e',['vector&lt; int &gt;',['../structtmbutils_1_1vector.html',1,'tmbutils']]],
-  ['vector_3c_20matrix2x2_20_3e',['vector&lt; matrix2x2 &gt;',['../structtmbutils_1_1vector.html',1,'tmbutils::vector&lt; matrix2x2 &gt;'],['../structvector.html',1,'vector&lt; matrix2x2 &gt;']]],
+  ['vector_3c_20matrix2x2_20_3e',['vector&lt; matrix2x2 &gt;',['../structvector.html',1,'vector&lt; matrix2x2 &gt;'],['../structtmbutils_1_1vector.html',1,'tmbutils::vector&lt; matrix2x2 &gt;']]],
   ['vector_3c_20mvnorm_5ft_3c_20scalartype_20_3e_20_3e',['vector&lt; MVNORM_t&lt; scalartype &gt; &gt;',['../structvector.html',1,'']]],
   ['vector_3c_20sphess_20_2a_20_3e',['vector&lt; sphess * &gt;',['../structvector.html',1,'']]],
   ['vector_3c_20vector_3c_20size_5ft_20_3e_20_3e',['vector&lt; vector&lt; size_t &gt; &gt;',['../structvector.html',1,'']]],
