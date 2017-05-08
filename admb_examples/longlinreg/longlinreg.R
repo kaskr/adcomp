427435e6e21b9ea3f1d12a529d705346d355944b
Kasper Kristensen <kaskr@dtu.dk>
2017-05-08 14:52:26 +0200

    Remove deprecated folders: - rm admb_examples - rm compare - rm slides

--- admb_examples/longlinreg/longlinreg.R
+++
@@ -1,6 +0,0 @@
-options("scipen"=100, "digits"=12)
-set.seed(123)
-nobs<-1000000
-x<-seq(0,10, length=nobs)
-cat(nobs,"\n",file="longlinreg.dat")
-write.table(cbind(2*x+1+rnorm(nobs),x), file="longlinreg.dat", row.names=FALSE, col.names=FALSE, append=TRUE)
