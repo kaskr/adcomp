408b5618a0c082b8a2f01644af2fcd7f26da734e
Travis CI <kaskr@dtu.dk>
2018-07-06 15:55:01 +0000

    Deploy to GitHub Pages: 8768ff9e28c3e6a9a162a2d93cba073a71b0d4d9

--- search/all_13.js
+++ search/all_13.js
@@ -1,11 +1,12 @@
 var searchData=
 [
   ['special_20matrix_20functions',['Special matrix functions',['../group__matrix__functions.html',1,'']]],
-  ['scale_5ft',['SCALE_t',['../classdensity_1_1SCALE__t.html',1,'density']]],
   ['scale_5ft',['SCALE_t',['../classSCALE__t.html',1,'']]],
+  ['scale_5ft',['SCALE_t',['../classdensity_1_1SCALE__t.html',1,'density']]],
   ['separable',['SEPARABLE',['../density_8hpp.html#afd76474bb73fa9af11b9dfb6c11f0c57',1,'SEPARABLE():&#160;density.hpp'],['../namespacedensity.html#a7163798c4b0467ac2e570b034d7ca4c5',1,'density::SEPARABLE()']]],
   ['separable_5ft',['SEPARABLE_t',['../classSEPARABLE__t.html',1,'']]],
   ['separable_5ft',['SEPARABLE_t',['../classdensity_1_1SEPARABLE__t.html',1,'density']]],
+  ['setdim',['setdim',['../structarray.html#a8a41ec12f07808c62e5b9848cebe9a63',1,'array::setdim()'],['../structtmbutils_1_1array.html#a1800f7ced31e5deb6f8864bc9369f5d1',1,'tmbutils::array::setdim()']]],
   ['simulate',['simulate',['../classAR1__t.html#ab50537f15c92d25cc378863c41b63367',1,'AR1_t::simulate()'],['../classdensity_1_1AR1__t.html#aa23a34e54a3cf7543208a2ad30f9f1ca',1,'density::AR1_t::simulate()'],['../atomic_8hpp.html#ad0e80df47530e00880f7396e7fde2782',1,'atomic::compois_utils::simulate()'],['../group__macros.html#ga6c7c2eba6072ba55386fe72bc49604e2',1,'SIMULATE():&#160;tmb_core.hpp']]],
   ['simulate_5fnot_5fyet_5fimplemented',['SIMULATE_NOT_YET_IMPLEMENTED',['../density_8hpp.html#a55f3dd2dbccf00be303b1d76567d2f22',1,'tmbutils_extra.hpp']]],
   ['simulation',['Simulation',['../Simulation.html',1,'']]],
--- search/functions_12.js
+++ search/functions_12.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['separable',['SEPARABLE',['../density_8hpp.html#afd76474bb73fa9af11b9dfb6c11f0c57',1,'SEPARABLE():&#160;density.hpp'],['../namespacedensity.html#a7163798c4b0467ac2e570b034d7ca4c5',1,'density::SEPARABLE()']]],
+  ['setdim',['setdim',['../structarray.html#a8a41ec12f07808c62e5b9848cebe9a63',1,'array::setdim()'],['../structtmbutils_1_1array.html#a1800f7ced31e5deb6f8864bc9369f5d1',1,'tmbutils::array::setdim()']]],
   ['simulate',['simulate',['../classAR1__t.html#ab50537f15c92d25cc378863c41b63367',1,'AR1_t::simulate()'],['../classdensity_1_1AR1__t.html#aa23a34e54a3cf7543208a2ad30f9f1ca',1,'density::AR1_t::simulate()'],['../atomic_8hpp.html#ad0e80df47530e00880f7396e7fde2782',1,'atomic::compois_utils::simulate()']]],
   ['split',['split',['../convenience_8hpp.html#a6f8b0640162d24d598e3f137ce43e2e8',1,'convenience.hpp']]],
   ['squeeze',['squeeze',['../convenience_8hpp.html#a60881c3c25c4c417f502545879c32d5e',1,'convenience.hpp']]],
