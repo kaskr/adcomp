3c213a1ad12430993b9ce00699a26f6a34cb10fc
Travis CI <kaskr@dtu.dk>
2018-12-10 07:56:20 +0000

    Deploy to GitHub Pages: fdc1a7a7cc3e460403d7c34f82f77050712bfb43

--- search/classes_9.js
+++ search/classes_9.js
@@ -5,5 +5,7 @@ var searchData=
   ['separable_5ft',['SEPARABLE_t',['../classSEPARABLE__t.html',1,'']]],
   ['separable_5ft',['SEPARABLE_t',['../classdensity_1_1SEPARABLE__t.html',1,'density']]],
   ['spde_5faniso_5ft',['spde_aniso_t',['../structR__inla_1_1spde__aniso__t.html',1,'R_inla']]],
-  ['spde_5ft',['spde_t',['../structR__inla_1_1spde__t.html',1,'R_inla']]]
+  ['spde_5ft',['spde_t',['../structR__inla_1_1spde__t.html',1,'R_inla']]],
+  ['splinefun',['splinefun',['../classtmbutils_1_1splinefun.html',1,'tmbutils']]],
+  ['splinefun',['splinefun',['../classsplinefun.html',1,'']]]
 ];
