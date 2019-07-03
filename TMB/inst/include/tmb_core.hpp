90b1a0aff02e27b70ce1ad800d117dbd7e6895af
Kasper Kristensen <kaskr@dtu.dk>
2019-07-03 13:49:50 +0200

    tmbad_usingatomics

--- TMB/inst/include/tmb_core.hpp
+++ TMB/inst/include/tmb_core.hpp
@@ -2019,7 +2019,15 @@ extern "C"
 
 extern "C"
 {
-  SEXP usingAtomics(){
+  SEXP TMBAD_usingAtomics(){
+    SEXP ans;
+    PROTECT(ans = Rf_allocVector(INTSXP,1));
+    INTEGER(ans)[0] = 1; // TMBAD doesn't benefit from knowing if 'false'
+    UNPROTECT(1);
+    return ans;
+  }
+
+  SEXP CPPAD_usingAtomics(){
     SEXP ans;
     PROTECT(ans = Rf_allocVector(INTSXP,1));
     INTEGER(ans)[0] = atomic::atomicFunctionGenerated;
@@ -2073,6 +2081,9 @@ extern "C"
   }
   SEXP MakeADGradObject(SEXP data, SEXP parameters, SEXP report) {
     return TMBAD_MakeADGradObject(data, parameters, report);
+  }
+  SEXP usingAtomics() {
+    return TMBAD_usingAtomics();
   }
 }
 
