2fd226ebf284c314c880fb99e219a519fd4486a8
Kasper Kristensen <kaskr@dtu.dk>
2019-07-19 13:14:15 +0200

    tmbad_atomic_macro: Add dummy template argument (breaks c++98 !!!)

--- TMB/inst/include/tmbad_atomic_macro.hpp
+++ TMB/inst/include/tmbad_atomic_macro.hpp
@@ -9,12 +9,15 @@
   ATOMIC_DOUBLE,                                                        \
   ATOMIC_REVERSE                                                        \
 )                                                                       \
+template<class dummy=void>                                              \
 CppAD::vector<TMBad::ad_aug> ATOMIC_NAME (const CppAD::vector<TMBad::ad_aug> &x); \
+template<class dummy=void>                                              \
 CppAD::vector<double> ATOMIC_NAME (const CppAD::vector<double> &tx) {   \
   CppAD::vector<double> ty(OUTPUT_DIM);                                 \
   ATOMIC_DOUBLE;                                                        \
   return ty;                                                            \
 }                                                                       \
+template<class dummy=void>                                              \
 struct ATOMIC_NAME ## Op : TMBad::global::DynamicInputOutputOperator {  \
   typedef TMBad::global::DynamicInputOutputOperator Base;               \
   ATOMIC_NAME ## Op (TMBad::Index n, TMBad::Index m) : Base(n, m) {}    \
@@ -46,10 +49,11 @@ struct ATOMIC_NAME ## Op : TMBad::global::DynamicInputOutputOperator {  \
   void forward(TMBad::ForwardArgs<TMBad::Writer> &args) { ASSERT(false); } \
   void reverse(TMBad::ReverseArgs<TMBad::Writer> &args) { ASSERT(false); } \
 };                                                                      \
+template<class dummy=void>                                              \
 CppAD::vector<TMBad::ad_aug> ATOMIC_NAME (const CppAD::vector<TMBad::ad_aug> &tx) { \
   TMBad::Index n = tx.size();                                           \
   TMBad::Index m = OUTPUT_DIM;                                          \
-  typedef ATOMIC_NAME ## Op OP;                                         \
+  typedef ATOMIC_NAME ## Op <> OP;                                      \
   TMBad::OperatorPure* pOp = TMBad::get_glob()->getOperator<OP>(n, m);  \
   std::vector<TMBad::ad_plain> x(&tx[0], &tx[0] + tx.size());           \
   std::vector<TMBad::ad_plain> y = TMBad::get_glob()->add_to_stack<OP>(pOp, x); \
@@ -57,6 +61,7 @@ CppAD::vector<TMBad::ad_aug> ATOMIC_NAME (const CppAD::vector<TMBad::ad_aug> &tx
   for (size_t i=0; i<y.size(); i++) ty[i] = y[i];                       \
   return ty;                                                            \
 }                                                                       \
+template<class dummy=void>                                              \
 void ATOMIC_NAME (const CppAD::vector<TMBad::ad_aug> &tx,               \
                   CppAD::vector<TMBad::ad_aug> &ty) {                   \
   ty = ATOMIC_NAME(tx);                                                 \
