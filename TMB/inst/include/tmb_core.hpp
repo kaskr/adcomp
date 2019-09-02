ea2b14827a8e3d565819ebdaa0a9e66c2ade71f8
Kasper Kristensen <kaskr@dtu.dk>
2019-09-02 15:32:18 +0200

    Allow no optimize after compress

--- TMB/inst/include/tmb_core.hpp
+++ TMB/inst/include/tmb_core.hpp
@@ -1512,7 +1512,7 @@ SEXP TMBAD_TransformADFunObject(SEXP f, SEXP control)
     }
     else if (method == 10) {
       TMBad::compress(pf->glob);
-      pf->glob.optimize();
+      if (config.optimize.instantly) pf->glob.optimize();
       TMBad::compile(pf->glob);
     }
     else if (method == 11)
