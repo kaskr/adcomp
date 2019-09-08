8b89003a2b930a73029a6b398c7140c06bca07ef
Kasper Kristensen <kaskr@dtu.dk>
2019-09-09 00:54:42 +0200

    oops

--- TMB/inst/include/tmb_core.hpp
+++ TMB/inst/include/tmb_core.hpp
@@ -2340,9 +2340,10 @@ extern "C"
   }
 }
 
-extern "C"
-{
-  // Select AD framework
+#endif /* #ifndef WITH_LIBTMB */
+
+
+// Select AD framework
 #ifdef TMBAD_FRAMEWORK
 #define MakeADFunObject      TMBAD_MakeADFunObject
 #define EvalADFunObject      TMBAD_EvalADFunObject
@@ -2360,8 +2361,6 @@ extern "C"
 #define TransformADFunObject CPPAD_TransformADFunObject
 #endif
 }
-
-#endif /* #ifndef WITH_LIBTMB */
 
 
 #ifdef WITH_LIBTMB
