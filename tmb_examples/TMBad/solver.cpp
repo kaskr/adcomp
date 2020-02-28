3eca94a80aa8819a15042d9583ab4afa9125de71
Kasper Kristensen <kaskr@dtu.dk>
2020-02-28 17:32:36 +0100

    Newton: Now works with sparse/dense

--- tmb_examples/TMBad/solver.cpp
+++ tmb_examples/TMBad/solver.cpp
@@ -1,40 +1,270 @@
 // Demonstrate adaptive solver of TMBad
 #include <TMB.hpp>
 
+/* =================== TODO ===================
+   - Optimize RefOp
+   - Cleanup get_segment mess
+   - Optimize sparsematrix CTOR: Only do 'setFromTriplets' once !
+   - Graph permutation: outer - inner trick.
+   - SparseMatrix: Diagonal must always be part of pattern !
+   - Configurable factorization type - add as template parameter to hessian_types
+   - Option for saddlepoint solver ?
+   - Move entire code to TMBad ?
+   - Interface considerations:
+   ---> In addition to solution one wants the hessian evaluated in the optimum. And function value.
+   ---> However, the type of the hessian depends on sparse/dense flag.
+   - 'hessian_type_dense' rename 'jacobian_dense_t' and similar
+   - Generalize HessianSolveVector to handle non-symmetric jacobian
+   ============================================
+*/
+
+/** Methods specific for a dense hessian */
+template<class Factorization=Eigen::LLT<Eigen::Matrix<double, -1, -1> > >
+struct jacobian_dense_t : TMBad::ADFun<> {
+  typedef TMBad::ADFun<> Base;
+  template<class T>
+  struct MatrixResult {
+    typedef matrix<T> type;
+  };
+  size_t n;
+  Factorization llt;
+  jacobian_dense_t() {}
+  // FIXME: Want const &G
+  // -->   JacFun, var2op, get_keep_var  -->  const
+  jacobian_dense_t(TMBad::ADFun<> &G, size_t n) :
+    n(n) {
+    Base::operator= ( G.JacFun() );
+  }
+  template<class T>
+  matrix<T> as_matrix(const std::vector<T> &Hx) {
+    return asMatrix(vector<T>(Hx), n, n);
+  }
+  template<class T>
+  std::vector<T> eval(const std::vector<T> &x) {
+    return Base::operator()(x);
+  }
+  template<class T>
+  matrix<T> operator()(const std::vector<T> &x) {
+    return as_matrix(Base::operator()(x));
+  }
+  /* Sparsity restricted cross product */
+  template<class T>
+  vector<T> crossprod(const vector<T> &y2, const vector<T> &y) {
+    matrix<T> ans = ( y2.matrix() * y.matrix().transpose() ).array();
+    return ans.vec();
+  }
+  // Sparse.factorize() == Dense.compute()
+  void llt_factorize(const matrix<TMBad::Scalar> &h) {
+    llt.compute(h);
+  }
+};
+/** Methods specific for a sparse hessian */
+template<class Factorization=Eigen::SimplicialLLT<Eigen::SparseMatrix<double> > >
+struct jacobian_sparse_t : TMBad::Sparse<TMBad::ADFun<> > {
+  typedef TMBad::Sparse<TMBad::ADFun<> > Base;
+  template<class T>
+  struct MatrixResult {
+    typedef Eigen::SparseMatrix<T> type;
+  };
+  size_t n;
+  // FIXME: llt.analyzePattern(H) on construct !!!
+  Factorization llt;
+  jacobian_sparse_t& operator=(const jacobian_sparse_t &other) {
+    Base::operator=(other);
+    n = other.n;
+    init_llt();
+    return *this;
+  }
+  jacobian_sparse_t (const jacobian_sparse_t &other) : Base(other), n(other.n) {
+    init_llt();
+  }
+  jacobian_sparse_t() {}
+  void init_llt() {
+    // Analyze pattern
+    std::vector<TMBad::Scalar> dummy(this->Range(), 0);
+    Eigen::SparseMatrix<TMBad::Scalar> H_dummy = as_matrix(dummy);
+    llt.analyzePattern(H_dummy);
+  }
+  // FIXME: G const !!!
+  jacobian_sparse_t(TMBad::ADFun<> &G, size_t n) :
+    n(n) {
+    Base::operator= (G.SpJacFun());
+    init_llt();
+  }
+  template<class T>
+  Eigen::SparseMatrix<T> as_matrix(const std::vector<T> &Hx) {
+    typedef Eigen::Triplet<T> T3;
+    std::vector<T3> tripletList;
+    size_t K = Hx.size();
+    for(size_t k=0; k<K; k++) {
+      tripletList.push_back( T3( Base::i[k],
+                                 Base::j[k],
+                                 Hx[k] ) );
+    }
+    Eigen::SparseMatrix<T> mat(n, n);
+    mat.setFromTriplets(tripletList.begin(), tripletList.end());
+    return mat;
+  }
+  template<class T>
+  std::vector<T> eval(const std::vector<T> &x) {
+    return Base::operator()(x);
+  }
+  template<class T>
+  Eigen::SparseMatrix<T> operator()(const std::vector<T> &x) {
+    std::vector<T> Hx = Base::operator()(x);
+    return as_matrix(Hx);
+  }
+  /* Sparsity restricted cross product */
+  template<class T>
+  vector<T> crossprod(const vector<T> &y2, const vector<T> &y) {
+    size_t nnz = Base::Range();
+    vector<T> ans(nnz);
+    for (size_t k=0; k<nnz; k++) {
+      ans[k] = y2[ Base::i [k] ] * y[ Base::j [k] ];
+    }
+    return ans;
+  }
+  // Sparse.factorize() == Dense.compute()
+  void llt_factorize(const Eigen::SparseMatrix<TMBad::Scalar> &h) {
+    llt.factorize(h);
+  }
+};
+
+template <class Hessian_Type>
+struct HessianSolveVector : TMBad::global::DynamicOperator< -1, -1 > {
+  static const bool have_input_size_output_size = true;
+  static const bool add_forward_replay_copy = true;
+  /** \warning Pointer */
+  Hessian_Type* hessian;
+  HessianSolveVector(Hessian_Type* hessian) : hessian(hessian) {}
+  TMBad::Index input_size() const { return hessian -> Range() + hessian -> n; }
+  TMBad::Index output_size() const { return hessian -> n; }
+  template<class T>
+  std::vector<T> get_segment(TMBad::ForwardArgs<T> &args, size_t from, size_t size) {
+    std::vector<T> ans(size);
+    for (size_t i=0; i<size; i++) ans[i] = args.x(from + i);
+    return ans;
+  }
+  template<class T>
+  std::vector<T> get_segment(TMBad::ReverseArgs<T> &args, size_t from, size_t size) {
+    std::vector<T> ans(size);
+    for (size_t i=0; i<size; i++) ans[i] = args.x(from + i);
+    return ans;
+  }
+  template<class T>
+  std::vector<T> get_segment_y(TMBad::ReverseArgs<T> &args, size_t from, size_t size) {
+    std::vector<T> ans(size);
+    for (size_t i=0; i<size; i++) ans[i] = args.y(from + i);
+    return ans;
+  }
+  vector<TMBad::Scalar> eval(const std::vector<TMBad::Scalar> &h,
+                             const vector<TMBad::Scalar> &x) {
+    typename Hessian_Type::template MatrixResult<TMBad::Scalar>::type
+      H = hessian -> as_matrix(h);
+    hessian -> llt_factorize(H); // Assuming analyzePattern(H) has been called once
+    vector<TMBad::Scalar> y = hessian -> llt.solve(x.matrix()).array();
+    return y;
+  }
+  vector<TMBad::Replay> eval(const std::vector<TMBad::Replay> &h,
+                             const std::vector<TMBad::Replay> &x) {
+    std::vector<TMBad::ad_plain> hx;
+    hx.insert(hx.end(), h.begin(), h.end());
+    hx.insert(hx.end(), x.begin(), x.end());
+    TMBad::global::Complete<HessianSolveVector> solve(*this);
+    std::vector<TMBad::ad_plain> ans = solve(hx);
+    std::vector<TMBad::ad_aug> ans2(ans.begin(), ans.end());
+    return ans2;
+  }
+  void forward(TMBad::ForwardArgs<TMBad::Scalar> &args) {
+    // FIXME: args.get_segment ???
+    size_t nnz = hessian -> Range();
+    size_t n = hessian -> n;
+    std::vector<TMBad::Scalar> h = get_segment(args, 0, nnz);
+    vector<TMBad::Scalar> x = get_segment(args, nnz, n);
+    vector<TMBad::Scalar> y = eval(h, x);
+    for (size_t i=0; i<n; i++) args.y(i) = y[i];
+  }
+  template <class T>
+  void reverse(TMBad::ReverseArgs<T> &args) {
+
+    size_t nnz = hessian -> Range();
+    size_t n = hessian -> n;
+    std::vector<T> h = get_segment(args, 0, nnz);
+    vector<T> y(n), dy(n);
+    for (size_t i=0; i<n; i++) {
+      y[i] = args.y(i);
+      dy[i] = args.dy(i);
+    }
+    vector<T> y2 = eval(h, dy);
+    vector<T> y2y = hessian->crossprod(y2, y);
+    for (size_t k=0; k<nnz; k++) {
+      args.dx(k) -= y2y[k];
+    }
+    for (size_t k=0; k<n; k++) {
+      args.dx(nnz + k) += y2[k];
+    }
+  }
+  template<class T>
+  void forward(TMBad::ForwardArgs<T> &args) { ASSERT(false); }
+  void reverse(TMBad::ReverseArgs<TMBad::Writer> &args) { ASSERT(false); }
+  const char* op_name() { return "JSolve"; }
+};
+
 struct newton_config {
   int maxit, trace;
   double grad_tol, step_tol, tol10, mgcmax, ustep, power, u0;
-  newton_config() :
-    maxit(1000), trace(0),
-    grad_tol(1e-8),
-    step_tol(1e-8),
-    tol10(0.001),
-    mgcmax(1e+60),
-    ustep(1),
-    power(0.5),
-    u0(1e-04)
-  {}
+  bool sparse;
+  void set_defaults(SEXP x = R_NilValue) {
+#define SET_DEFAULT(name, value) set_from_real(x, name, #name, value)
+    SET_DEFAULT(maxit, 1000);
+    SET_DEFAULT(trace, 0);
+    SET_DEFAULT(grad_tol, 1e-8);
+    SET_DEFAULT(step_tol, 1e-8);
+    SET_DEFAULT(tol10, 0.001);
+    SET_DEFAULT(mgcmax, 1e+60);
+    SET_DEFAULT(ustep, 1);
+    SET_DEFAULT(power, 0.5);
+    SET_DEFAULT(u0, 1e-04);
+    SET_DEFAULT(sparse, false);
+#undef SET_DEFAULT
+  }
+  newton_config() {
+    set_defaults();
+  }
+  newton_config(SEXP x) {
+    set_defaults(x);
+  }
+  template<class T>
+  void set_from_real(SEXP x, T &target, const char* name, double value) {
+    SEXP y = getListElement(x, name);
+    target = (T) ( y != R_NilValue ? REAL(y)[0] : value );
+  }
+};
+template <class Type>
+struct newton_config_t : newton_config {
+  newton_config_t() : newton_config() {}
+  newton_config_t(SEXP x) : newton_config(x) {}
 };
 
 /* Generalized newton solver similar to R function TMB:::newton */
-template<class Functor, class Type>
-struct NewtonSolver : TMBad::global::SharedDynamicOperator {
+template<class Functor, class Type, class Hessian_Type=jacobian_dense_t<> >
+struct NewtonOperator : TMBad::global::SharedDynamicOperator {
   static const bool have_input_size_output_size = true;
   static const bool add_forward_replay_copy = true;
   typedef TMBad::Scalar Scalar;
   typedef TMBad::StdWrap<Functor, vector<TMBad::ad_aug> > FunctorExtend;
-  // FIXME: For now consider the dense hessian case
-  TMBad::ADFun<> function, gradient, hessian;
+  TMBad::ADFun<> function, gradient;
+  Hessian_Type hessian;
   // Control convergence
   newton_config cfg;
-  NewtonSolver(Functor &F, vector<Type> start, newton_config cfg)
+  NewtonOperator(Functor &F, vector<Type> start, newton_config cfg)
     : cfg(cfg)
   {
     function = TMBad::ADFun<> ( FunctorExtend(F), start);
     function.optimize();
     gradient = function.JacFun();
     gradient.optimize();
-    hessian = gradient.JacFun();
+    hessian = Hessian_Type(gradient, start.size());
     hessian.optimize();
   }
   // Helper to swap inner/outer
@@ -50,17 +280,21 @@ struct NewtonSolver : TMBad::global::SharedDynamicOperator {
   }
   // Put it self on tape
   vector<TMBad::ad_aug> add_to_tape() {
-    std::vector<TMBad::ad_aug> x = function.resolve_refs();
-    std::vector<TMBad::ad_aug> y = gradient.resolve_refs();
-    std::vector<TMBad::ad_aug> z = hessian.resolve_refs();
-    std::vector<TMBad::ad_plain> xyz;
-    xyz.insert(xyz.end(), x.begin(), x.end());
-    xyz.insert(xyz.end(), y.begin(), y.end());
-    xyz.insert(xyz.end(), z.begin(), z.end());
-    TMBad::global::Complete<NewtonSolver> solver(*this);
-    std::vector<TMBad::ad_plain> ans = solver(xyz);
-    std::vector<TMBad::ad_aug> ans2(ans.begin(), ans.end());
-    return ans2;
+    // Find outer parameters
+    std::vector<TMBad::ad_aug> f_par = function.resolve_refs();
+    std::vector<TMBad::ad_aug> g_par = gradient.resolve_refs();
+    std::vector<TMBad::ad_aug> h_par = hessian.resolve_refs();
+    // Append to 'full_par'
+    std::vector<TMBad::ad_aug> full_par;
+    full_par.insert(full_par.end(), f_par.begin(), f_par.end());
+    full_par.insert(full_par.end(), g_par.begin(), g_par.end());
+    full_par.insert(full_par.end(), h_par.begin(), h_par.end());
+    // Solver: input outer par -> output inner par
+    TMBad::global::Complete<NewtonOperator> solver(*this);
+    std::vector<TMBad::ad_aug> sol = solver(full_par);
+    // Append solution to full_par
+    full_par.insert(full_par.end(), sol.begin(), sol.end());
+    return full_par;
   }
   /* From TMB::newton */
   double phi(double u) {
@@ -83,7 +317,6 @@ struct NewtonSolver : TMBad::global::SharedDynamicOperator {
       if (function(x_start)[0] < function(x)[0])
         x = x_start;
     }
-    Eigen::LLT<Eigen::Matrix<double, -1, -1> > llt;
     Scalar f_previous = INFINITY;
     for (int i=0; i < cfg.maxit; i++) {
       vector<Scalar> g = gradient(x);
@@ -102,29 +335,33 @@ struct NewtonSolver : TMBad::global::SharedDynamicOperator {
         if (cfg.trace) std::cout << "\n";
         return;
       }
-      vector<Scalar> h = hessian(x);
+      typename Hessian_Type::template MatrixResult<TMBad::Scalar>::type
+        H = hessian(std::vector<Scalar>(x));
       if (cfg.trace) std::cout << "ustep=" << cfg.ustep << " ";
-      while (true) {
-        matrix<Scalar> hm = asMatrix(h, x.size(), x.size());
-        hm.diagonal().array() += phi( cfg.ustep );
-        llt.compute(hm);
-        if (cfg.trace) std::cout << "info=" << llt.info() << " ";
-        if (llt.info() == 0) break;
+      vector<Scalar> diag_cpy = H.diagonal().array();
+      while (true) { // FIXME: Infinite loop
+        // H := H + phi * I
+        H.diagonal().array() = diag_cpy;
+        H.diagonal().array() += phi( cfg.ustep );
+        // Try to factorize
+        hessian.llt_factorize(H);
+        if (hessian.llt.info() == 0) break;
+        // H not PD ==> Decrease phi
         cfg.ustep = decrease(cfg.ustep);
       }
       // We now have a PD hessian
       // Let's take a newton step and see if it improves...
-      matrix<Scalar> step = llt.solve( g.matrix() );
-      x = x - step.array();
-      Scalar f = function(x)[0];
-      if (f < f_previous + 1e-8) {
-        // Improvement
+      vector<Scalar> x_new =
+        x - hessian.llt.solve( g.matrix() ).array();
+      Scalar f = function(x_new)[0];
+      if (f < f_previous + 1e-8) { // Improvement
+        // Accept
         cfg.ustep = increase(cfg.ustep);
         f_previous = f;
-      } else {
+        x = x_new;
+      } else { // No improvement
         // Reject
         cfg.ustep = decrease(cfg.ustep);
-        x = x + step.array();
       }
       if (cfg.trace) std::cout << "f=" << f << " ";
       if (cfg.trace) std::cout << "\n";
@@ -173,8 +410,6 @@ struct NewtonSolver : TMBad::global::SharedDynamicOperator {
     for (size_t i=0; i < (size_t) sol.size(); i++) args.y(i) = sol[i];
   }
   template<class T>
-  void forward(TMBad::ForwardArgs<T> &args) { ASSERT(false); }
-  template<class T>
   void reverse(TMBad::ReverseArgs<T> &args) {
     vector<T> w(output_size());
     for (size_t i=0; i < (size_t) w.size(); i++) w[i] = args.dy(i);
@@ -188,31 +423,96 @@ struct NewtonSolver : TMBad::global::SharedDynamicOperator {
     std::vector<T> y = get_segment(args,    n1, n2);
     std::vector<T> z = get_segment(args, n1+n2, n3);
     std::vector<T> sol_z = sol; sol_z.insert(sol_z.end(), z.begin(), z.end());
-    vector<T> h = hessian(sol_z);
-    matrix<T> hm = asMatrix(h, sol.size(), sol.size());
-    vector<T> w2 = - atomic::matinv(hm) * w.matrix();
+    HessianSolveVector<Hessian_Type> solve(&hessian);
+    std::vector<T> hv = hessian.eval(sol_z);
+    vector<T> w2 = - solve.eval(hv, w);
     std::vector<T> sol_y = sol; sol_y.insert(sol_y.end(), y.begin(), y.end());
     vector<T> g = gradient.Jacobian(sol_y, w2);
     vector<T> g_outer = g.tail(n2);
     for (size_t i=0; i < (size_t) g_outer.size(); i++) args.dx(i) += g_outer[i];
   }
+  template<class T>
+  void forward(TMBad::ForwardArgs<T> &args) { ASSERT(false); }
   void reverse(TMBad::ReverseArgs<TMBad::Writer> &args) { ASSERT(false); }
   const char* op_name() { return "Newton"; }
 };
 
-template<class Functor>
-vector<TMBad::ad_aug> Newton(Functor &F, vector<TMBad::ad_aug> start,
-                             newton_config cfg = newton_config() ) {
-  NewtonSolver<Functor, TMBad::ad_aug > NS(F, start, cfg);
-  return NS.add_to_tape();
+// Interface
+template<class Newton>
+void Newton_CTOR_Hook(Newton &F, const vector<double> &start) {
+  F.sol = start;
+  F.newton_iterate(F.sol);
 }
-template<class Functor>
-vector<double> Newton(Functor &F, vector<double> start,
-                      newton_config cfg = newton_config() ) {
-  NewtonSolver<Functor, TMBad::ad_aug > NS(F, start, cfg);
-  vector<double> x = start;
-  NS.newton_iterate(x);
-  return x;
+template<class Newton>
+void Newton_CTOR_Hook(Newton &F, const vector<TMBad::ad_aug> &start) {
+  F.sol = F.add_to_tape();
+}
+template<class Functor, class Type, class Hessian_Type=jacobian_dense_t<> >
+struct NewtonSolver : NewtonOperator<Functor, TMBad::ad_aug, Hessian_Type > {
+  typedef NewtonOperator<Functor, TMBad::ad_aug, Hessian_Type > Base;
+  typedef typename Hessian_Type::template MatrixResult<Type>::type hessian_t;
+  vector<Type> sol; // c(f_par, g_par, h_par, sol)
+  size_t n; // Number of inner parameters
+  NewtonSolver(Functor &F, vector<Type> start, newton_config cfg) :
+    Base(F, start, cfg), n(start.size()) {
+    // sol = start;
+    // Base::newton_iterate(sol);
+    Newton_CTOR_Hook(*this, start);
+  }
+  // Get solution
+  operator vector<Type>() const {
+    return sol.tail(n);
+  }
+  vector<Type> solution() {
+    return sol.tail(n);
+  }
+  Type value() {
+    // Construct c(sol, f_par)
+    size_t n1 = Base::function.DomainOuter();
+    vector<Type> f_par_sol(n1 + n);
+    f_par_sol << sol.tail(n), sol.head(n1);
+    return Base::function(std::vector<Type>(f_par_sol))[0];
+  }
+  hessian_t hessian() {
+    // Construct c(sol, h_par)
+    size_t n1 = Base::function.DomainOuter();
+    size_t n2 = Base::gradient.DomainOuter();
+    size_t n3 = Base::hessian.DomainOuter();
+    vector<Type> h_par_sol(n3 + n);
+    h_par_sol << sol.tail(n), sol.segment(n1+n2,n3);
+    return Base::hessian(std::vector<Type>(h_par_sol));
+  }
+};
+
+template<class Functor, class Type>
+NewtonSolver<Functor,
+             Type,
+             jacobian_sparse_t<> > NewtonSparse(
+                                                Functor &F,
+                                                vector<Type> start,
+                                                newton_config cfg = newton_config() ) {
+  NewtonSolver<Functor, Type, jacobian_sparse_t<> > ans(F, start, cfg);
+  return ans;
+}
+
+template<class Functor, class Type>
+NewtonSolver<Functor,
+             Type,
+             jacobian_dense_t<> > NewtonDense(
+                                                Functor &F,
+                                                vector<Type> start,
+                                                newton_config cfg = newton_config() ) {
+  NewtonSolver<Functor, Type, jacobian_dense_t<> > ans(F, start, cfg);
+  return ans;
+}
+
+template<class Functor, class Type>
+vector<Type> Newton(Functor &F, vector<Type> start,
+                    newton_config cfg = newton_config() ) {
+  if (cfg.sparse)
+    return NewtonSparse(F, start, cfg);
+  else
+    return NewtonDense(F, start, cfg);
 }
 
 /*
@@ -240,11 +540,12 @@ struct Functor {
 template<class Type>
 Type objective_function<Type>::operator() ()
 {
-  DATA_INTEGER(trace);
   DATA_MATRIX(m);
   PARAMETER_VECTOR(x);
   Functor<TMBad::ad_aug> F(m, x);
-  newton_config cfg; cfg.trace = trace;
+  DATA_STRUCT(cfg, newton_config_t);
   vector<Type> sol = Newton(F, x, cfg);
-  return sol.sum();
+  Type nll = 0;
+  nll += sol.sum();
+  return nll;
 }
