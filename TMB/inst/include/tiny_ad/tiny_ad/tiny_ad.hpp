e250f43b322e7b9d36ba322f57b50b646367d1eb
Kasper Kristensen <kaskr@dtu.dk>
2021-03-17 17:39:33 +0100

    tiny_ad variable: Remove CTOR from any type - Problematic CTOR was introduced in commit d4e42fe0fcf47fc2e1bb8d8abe1cb3fb82c4953c to aid implementation of compois special functions. - Later Eigen update made it illegal to use general CTOR from any type (as was seen in a similar issue solved by commit e3af7ae165803f0f8ecc680f5e504591e8eb73d4)

--- TMB/inst/include/tiny_ad/tiny_ad/tiny_ad.hpp
+++ TMB/inst/include/tiny_ad/tiny_ad/tiny_ad.hpp
@@ -333,12 +333,21 @@ struct traits<1, nvar, Double> {
     variable(double x, int id) : Base(x) {
       setid(id);
     }
-    template<class Constant>
-    variable(Constant x) {
+    template<int T1, int T2, class T3>
+    variable(variable<T1,T2,T3> x) {
       Base::value = x; Base::deriv.setZero();
     }
-    template<class Constant>
-    variable(Constant x, int id) {
+    template<class T1, class T2>
+    variable(ad<T1,T2> x) {
+      Base::value = x; Base::deriv.setZero();
+    }
+    template<int T1, int T2, class T3>
+    variable(variable<T1,T2,T3> x, int id) {
+      Base::value = x; Base::deriv.setZero();
+      setid(id);
+    }
+    template<class T1, class T2>
+    variable(ad<T1,T2> x, int id) {
       Base::value = x; Base::deriv.setZero();
       setid(id);
     }
