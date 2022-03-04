3672e0570978557ed6a44527d61c8755ce7027f6
Travis CI <kaskr@dtu.dk>
2022-03-04 13:13:25 +0000

    Deploy to GitHub Pages: 79ea7067f7dd90a53060942f34aa9a06059f0ce3

--- search/classes_d.js
+++ search/classes_d.js
@@ -9,8 +9,8 @@ var searchData=
   ['spde_5faniso_5ft',['spde_aniso_t',['../structR__inla_1_1spde__aniso__t.html',1,'R_inla']]],
   ['spde_5ft',['spde_t',['../structR__inla_1_1spde__t.html',1,'R_inla']]],
   ['spjacfun_5fconfig',['SpJacFun_config',['../structTMBad_1_1SpJacFun__config.html',1,'TMBad']]],
-  ['splinefun',['splinefun',['../classtmbutils_1_1splinefun.html',1,'tmbutils']]],
   ['splinefun',['splinefun',['../classsplinefun.html',1,'']]],
+  ['splinefun',['splinefun',['../classtmbutils_1_1splinefun.html',1,'tmbutils']]],
   ['sr_5fgrid',['sr_grid',['../structTMBad_1_1sr__grid.html',1,'TMBad']]],
   ['stdwrap',['StdWrap',['../structTMBad_1_1StdWrap.html',1,'TMBad']]]
 ];
