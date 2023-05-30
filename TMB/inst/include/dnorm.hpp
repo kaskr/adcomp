55b069dc28d813a407df488498001b1d5bcb0fa8
Kasper Kristensen <kaskr@dtu.dk>
2023-05-30 15:38:52 +0200

    dnorm: extra type cast (so works for complex)

--- TMB/inst/include/dnorm.hpp
+++ TMB/inst/include/dnorm.hpp
@@ -17,7 +17,7 @@ template<class Type>
 Type dnorm(Type x, Type mean, Type sd, int give_log=0)
 {
   Type resid = (x - mean) / sd;
-  Type logans = -log(sqrt(2*M_PI)) - log(sd) - Type(.5) * resid * resid;
+  Type logans = Type(-log(sqrt(2*M_PI))) - log(sd) - Type(.5) * resid * resid;
   if(give_log) return logans; else return exp(logans);
 }
 VECTORIZE4_ttti(dnorm)
